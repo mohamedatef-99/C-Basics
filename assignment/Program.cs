47545cd8d76b09a65e4060a20c7168de8075cc93
Mohamed Atef <mohammed.atef.saif@gmail.com>
2024-12-13 11:28:51 +0200

    Variable Decleration

--- assignment/Program.cs
+++ assignment/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Variable Decliration
+            //// DataType variableName
+            //int number;
+            ////allocate unintialized 4 bytes of memory
+            ////Console.WriteLine(number); invalid
+            //number = 10;
+            ////Console.WriteLine(number);  10
+
+            //int number2 = 20; //declare and initialize
+            //Console.WriteLine(number2); 
+            #endregion
+
+
+
+
         }
     }
 }
